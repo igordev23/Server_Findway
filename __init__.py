942d1e8068d2a526d7703180fb62655f472afd68
igordev23 <igor.dev89898@gmail.com>
2025-07-02 17:32:42 -0300

    Ajustando com banco de dados com routes correto

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ def create_app():
     app = Flask(__name__,  static_folder='static', template_folder='templates')
     app.config.from_object('config.Config')
 
-    from app.routes.gps_routes import bp as gps_bp
+    from routes.gps_routes import bp as gps_bp
     app.register_blueprint(gps_bp)
 
     return app
