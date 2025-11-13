6ba708d4ce917d128ba745bc75d65eb77f895534
larissaNa <larysou66@gmail.com>
2025-11-13 16:49:01 -0300

    ajustando gitignore e __init__

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__,  static_folder='static', template_folder='templates')
+    app.config["ASSETS_ROOT"] = "/static"
     app.config.from_object('config.Config')
 
     from app.routes.gps_routes import bp as gps_bp
