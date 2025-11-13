04c0fe139b0f974b94aa43cf904d7280823174ec
Isabellybrt <isabellybrt77@gmail.com>
2025-11-13 17:27:08 -0300

    Corrigindo os models

--- models/administrador.py
+++ models/administrador.py
@@ -1,16 +1,20 @@
 from database import db
-from .usuario import Usuario
+from models.usuario import Usuario
 
 class Administrador(Usuario):
     __tablename__ = "Administrador"
-
     id = db.Column(db.BigInteger, db.ForeignKey("Usuario.id"), primary_key=True)
 
-    clientes = db.relationship("Cliente", backref="administrador", lazy=True)
+    clientes = db.relationship(
+        "Cliente",
+        backref="administrador",
+        lazy=True,
+        foreign_keys="Cliente.administrador_id"  # 👈 evita ambiguidade
+    )
 
     __mapper_args__ = {
         "polymorphic_identity": "administrador",
     }
 
     def __repr__(self):
-        return f"<Administrador {self.nome}>"
+        return f"<Administrador {self.id}>"
